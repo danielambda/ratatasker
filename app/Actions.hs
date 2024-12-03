c761962b91580b766327be34d059f425c9e36c8a
danielambda <daniel.gevorgyan25@gmail.com>
2024-12-03 05:57:42 +0300

    refactor: moved actions to separate file

---
+++ app/Actions.hs
@@ -0,0 +1,29 @@
+module Actions where
+
+import Data.Text (Text)
+
+actWhen :: Bool -> Action -> Action
+actWhen False = const NoAction
+actWhen True = id
+
+actWhenF :: Functor f => Bool -> f Action -> f Action
+actWhenF b = fmap $ actWhen b
+
+actUnlessF :: Functor f => Bool -> f Action -> f Action
+actUnlessF b = fmap $ actWhen $ not b
+
+actOnJust :: (a -> Action) -> Maybe a -> Action
+actOnJust f (Just a) = f a
+actOnJust _ Nothing = NoAction
+
+actOnJustM :: Monad m => (a -> m Action) -> Maybe a -> m Action
+actOnJustM f (Just a) = f a
+actOnJustM _ Nothing = pure NoAction
+
+data Action
+  = NoAction
+  | AddTasks [Text]
+  | CompleteTask Text
+  | SetTasksHeader Text
+  | SetNoTasksText Text
+
