d42e36140cf8fa3d62a1957615b6e146649a909b
danielambda <daniel.gevorgyan25@gmail.com>
2024-12-11 01:59:40 +0300

    refactor: joined dublicated code in persistence

--- app/Persistence.hs
+++ app/Persistence.hs
@@ -8,6 +8,7 @@ import Models
 import Database.SQLite.Simple
 import Data.Text (Text)
 import Telegram.Bot.API.Types hiding (User, UserId)
+import Data.Maybe (listToMaybe)
 
 initDb :: Connection -> IO ()
 initDb conn = do
@@ -34,61 +35,68 @@ addTask :: Connection -> UserId -> Text -> IO ()
 addTask conn (UserId (ChatId chatId) mThreadIdWrapped) task =
   let mThreadId = fmap (\(MessageThreadId m) -> m) mThreadIdWrapped
   in execute conn
-    "INSERT INTO tasks\
-    \(userChatId, userMessageThreadId, text) VALUES (?, ?, ?)"
+    "INSERT INTO tasks (userChatId, userMessageThreadId, text) VALUES (?, ?, ?)"
     (chatId, mThreadId, task)
 
 deleteTask :: Connection -> UserId -> Text -> IO ()
-deleteTask conn (UserId (ChatId chatId) (Just (MessageThreadId threadId))) task =
-  execute conn
-    "DELETE FROM tasks \
-    \WHERE userChatId = ? AND userMessageThreadId = ? AND text = ?"
-    (chatId, threadId, task)
-deleteTask conn (UserId (ChatId chatId) Nothing) task =
-  execute conn
-    "DELETE FROM tasks \
-    \WHERE userChatId = ? AND userMessageThreadId IS NULL AND text = ?"
-    (chatId, task)
+deleteTask conn (UserId (ChatId chatId) mThreadId) task =
+  case mThreadId of
+    Just (MessageThreadId threadId) -> execute conn
+      "DELETE FROM tasks WHERE userChatId = ? AND userMessageThreadId = ? AND text = ?"
+      (chatId, threadId, task)
+    Nothing -> execute conn
+      "DELETE FROM tasks WHERE userChatId = ? AND userMessageThreadId IS NULL AND text = ?"
+      (chatId, task)
 
 updateTasksHeader :: Connection -> UserId -> Text -> IO ()
-updateTasksHeader conn (UserId (ChatId chatId) Nothing) header =
-  execute conn
-    "UPDATE users SET tasksHeader = ? WHERE chatId = ? AND messageThreadId IS NULL"
-    (header, chatId)
-updateTasksHeader conn (UserId (ChatId chatId) (Just (MessageThreadId threadId))) header =
-  execute conn
-    "UPDATE users SET tasksHeader = ? WHERE chatId = ? AND messageThreadId = ?"
-    (header, chatId, threadId)
+updateTasksHeader conn (UserId (ChatId chatId) mThreadId) header =
+  case mThreadId of
+    Just (MessageThreadId threadId) -> execute conn
+      "UPDATE users SET tasksHeader = ? WHERE chatId = ? AND messageThreadId = ?"
+      (header, chatId, threadId)
+    Nothing -> execute conn
+      "UPDATE users SET tasksHeader = ? WHERE chatId = ? AND messageThreadId IS NULL"
+      (header, chatId)
 
 updateNoTasksText :: Connection -> UserId -> Text -> IO ()
-updateNoTasksText conn (UserId (ChatId chatId) Nothing) txt =
-  execute conn
-    "UPDATE users SET noTasksText = ? WHERE chatId = ? AND messageThreadId IS NULL"
-    (txt, chatId)
-updateNoTasksText conn (UserId (ChatId chatId) (Just (MessageThreadId threadId))) txt =
-  execute conn
-    "UPDATE users SET noTasksText = ? WHERE chatId = ? AND messageThreadId = ?"
-    (txt, chatId, threadId)
+updateNoTasksText conn (UserId (ChatId chatId) mThreadId) txt =
+  case mThreadId of
+    Just (MessageThreadId threadId) -> execute conn
+      "UPDATE users SET noTasksText = ? WHERE chatId = ? AND messageThreadId = ?"
+      (txt, chatId, threadId)
+    Nothing -> execute conn
+      "UPDATE users SET noTasksText = ? WHERE chatId = ? AND messageThreadId IS NULL"
+      (txt, chatId)
 
 updateMainMessageId :: Connection -> UserId -> MessageId -> IO ()
-updateMainMessageId conn (UserId (ChatId chatId) Nothing) (MessageId msgId) =
-  execute conn
-    "UPDATE users SET mainMessageId = ? WHERE chatId = ? AND messageThreadId IS NULL"
-    (msgId, chatId)
-updateMainMessageId conn (UserId (ChatId chatId) (Just (MessageThreadId threadId))) (MessageId msgId) =
-  execute conn
-    "UPDATE users SET mainMessageId = ? WHERE chatId = ? AND messageThreadId = ?"
-    (msgId, chatId, threadId)
+updateMainMessageId conn (UserId (ChatId chatId) mThreadId) (MessageId msgId) =
+  case mThreadId of
+    Just (MessageThreadId threadId) -> execute conn
+      "UPDATE users SET mainMessageId = ? WHERE chatId = ? AND messageThreadId = ?"
+      (msgId, chatId, threadId)
+    Nothing -> execute conn
+      "UPDATE users SET mainMessageId = ? WHERE chatId = ? AND messageThreadId IS NULL"
+      (msgId, chatId)
 
 getTasks :: Connection -> UserId -> IO [Text]
-getTasks conn (UserId (ChatId chatId) Nothing) =
-  fmap fromOnly <$> query conn
-    "SELECT text FROM tasks WHERE userChatId = ? AND userMessageThreadId IS NULL"
-    (Only chatId)
-getTasks conn (UserId (ChatId chatId) (Just (MessageThreadId threadId))) =
-  fmap fromOnly <$> query conn
-    "SELECT text FROM tasks WHERE userChatId = ? AND userMessageThreadId = ?"
-    (chatId, threadId)
+getTasks conn (UserId (ChatId chatId) mThreadId) =
+  fmap fromOnly <$> case mThreadId of
+    Just (MessageThreadId threadId) -> query conn
+      "SELECT text FROM tasks WHERE userChatId = ? AND userMessageThreadId = ?"
+      (chatId, threadId)
+    Nothing -> query conn
+      "SELECT text FROM tasks WHERE userChatId = ? AND userMessageThreadId IS NULL"
+      (Only chatId)
+
+getMainMessageId :: Connection -> UserId -> IO (Maybe MessageId)
+getMainMessageId conn (UserId (ChatId chatId) mThreadId) =
+  fmap listToMaybe $ fmap (MessageId . fromOnly) <$> case mThreadId of
+    Just (MessageThreadId threadId) -> query conn
+      "SELECT mainMessageId FROM users WHERE chatId = ? AND userMessageThreadId = ?"
+      (chatId, threadId)
+    Nothing -> query conn
+      "SELECT mainMessageId FROM users WHERE chatId = ? AND userMessageThreadId IS NULL"
+      (Only chatId)
 
 getUserWithId :: Connection -> UserId -> IO (Maybe User)
 getUserWithId conn userId@(UserId (ChatId chatId) mThreadId) =
@@ -107,19 +115,22 @@ getUserWithId conn userId@(UserId (ChatId chatId) mThreadId) =
     queryUserWithoutThreadId = query conn
       "SELECT \
       \mainMessageId, noTasksText, tasksHeader \
-      \FROM users WHERE chatid = ? AND messageThreadId IS NULL"
+      \FROM users WHERE chatId = ? AND messageThreadId IS NULL"
       (Only chatId)
 
     queryUserWithThreadId (MessageThreadId threadId) = query conn
       "SELECT \
       \mainMessageId, noTasksText, tasksHeader \
-      \FROM users WHERE chatid = ? AND messageThreadId = ?"
+      \FROM users WHERE chatId = ? AND messageThreadId = ?"
       (chatId, threadId)
 
 createEmptyUser :: Connection -> UserId -> MessageId -> VisualConfig -> IO User
-createEmptyUser conn
+createEmptyUser
+  conn
   (UserId (ChatId chatId) mThreadIdWrapped)
-  (MessageId mainMsgId) (VisualConfig noTasksText header) = do
+  (MessageId mainMsgId)
+  (VisualConfig noTasksText header)
+  = do
   let mThreadId = (\(MessageThreadId x) -> x) <$> mThreadIdWrapped
   execute conn
     "INSERT INTO users \
